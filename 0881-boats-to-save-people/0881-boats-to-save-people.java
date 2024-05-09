168fffd80ab5c9dfd5e7ba976938ad8c40b7693b
Franck <franckfongang99@gmail.com>
2024-05-09 22:54:57 +0000

    Time: 16 ms (--%), Space: 53.7 MB (--%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int count = 0, l = 0, r = people.length - 1;
+        Arrays.sort(people);
+
+        while (l <= r) {
+            if (people[r] == limit) {
+                count++;
+                r--;
+            } else if (people[l] + people[r] <= limit){
+                l++;
+                r--;
+                count++;
+            } else {
+                r--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
