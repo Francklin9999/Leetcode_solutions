b7916b8f0a3bd1ab07254a4c8555c4fc41e616c8
Franck <franckfongang99@gmail.com>
2024-05-09 01:25:18 +0000

    Time: 1 ms (--%), Space: 41.2 MB (--%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -2,14 +2,11 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> maxheap = new PriorityQueue<>(Comparator.reverseOrder());
         for (int stone : stones) maxheap.add(stone);
-        int y;
-        int x;
         while (maxheap.size() > 1) {
-            y = maxheap.poll();
-            x = maxheap.poll();
+            int y = maxheap.poll();
+            int x = maxheap.poll();
             if (x == y) continue;
-            y = y - x;
-            maxheap.offer(y);
+            maxheap.offer(y - x);
         }
         if (maxheap.size() == 0) return 0;
         return maxheap.poll();
