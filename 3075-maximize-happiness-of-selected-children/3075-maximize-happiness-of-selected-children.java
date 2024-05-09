c571e1d70c5a68abf56be0e37adbafec3881ea07
Franck <franckfongang99@gmail.com>
2024-05-09 06:39:42 +0000

    Time: 144 ms (--%), Space: 74.4 MB (--%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Comparator.reverseOrder());
+        long anw = 0;
+
+        for (int i = 0; i < happiness.length; i++) {
+            maxheap.add(happiness[i]);
+        }
+        for (int i = 0; i < k; i++) {
+            int temp = maxheap.poll() - i;
+            if (temp > 0) anw += temp;
+        }
+        return anw;
+    }
+}
