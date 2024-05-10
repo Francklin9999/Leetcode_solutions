2dd129583d3cde6f9098a39de93d54087b2881c6
Franck <franckfongang99@gmail.com>
2024-05-10 18:40:20 +0000

    Time: 10 ms (--%), Space: 49.2 MB (--%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -13,16 +13,16 @@ class Solution {
                 pair[1] = j;
                 queue.add(pair);
                 while (!queue.isEmpty()) {
-                        int[] curr = queue.poll();
-                        int r = curr[0];
-                        int c = curr[1];
-                        int[][] neighbords = {{r + 1, c}, {r, c + 1}, {r - 1, c}, {r, c - 1}};
-                        for (int z = 0; z < 4; z++) {
-                            int newR = neighbords[z][0], newL = neighbords[z][1];
-                            if (Math.min(newR, newL) < 0 || newR == ROWS || newL == COLMN ||
-                            grid[newR][newL] == '0') continue;
-                            queue.offer(neighbords[z]);
-                            grid[newR][newL] = '0';
+                    int[] curr = queue.poll();
+                    int r = curr[0];
+                    int c = curr[1];
+                    int[][] neighbords = {{r + 1, c}, {r, c + 1}, {r - 1, c}, {r, c - 1}};
+                    for (int z = 0; z < 4; z++) {
+                        int newR = neighbords[z][0], newL = neighbords[z][1];
+                        if (Math.min(newR, newL) < 0 || newR == ROWS || newL == COLMN ||
+                        grid[newR][newL] == '0') continue;
+                        queue.offer(neighbords[z]);
+                        grid[newR][newL] = '0';
                     }
                 }
             }
