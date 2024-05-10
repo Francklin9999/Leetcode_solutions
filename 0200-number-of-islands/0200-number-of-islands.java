ca50a6e03010bfb463d8731993842116e1e7b726
Franck <franckfongang99@gmail.com>
2024-05-10 02:29:05 +0000

    Time: 10 ms (--%), Space: 48.9 MB (--%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -13,12 +13,9 @@ class Solution {
                 pair[1] = j;
                 queue.add(pair);
                 while (!queue.isEmpty()) {
-                    int size = queue.size();
-                    for (int k = 0; k < size; k++) {
                         int[] curr = queue.poll();
                         int r = curr[0];
                         int c = curr[1];
-                        // if (grid[r][c] == '0') continue;
                         int[][] neighbords = {{r + 1, c}, {r, c + 1}, {r - 1, c}, {r, c - 1}};
                         for (int z = 0; z < 4; z++) {
                             int newR = neighbords[z][0], newL = neighbords[z][1];
@@ -26,7 +23,6 @@ class Solution {
                             grid[newR][newL] == '0') continue;
                             queue.offer(neighbords[z]);
                             grid[newR][newL] = '0';
-                        }
                     }
                 }
             }
