588762f50fb1a56deeb217a70c2bdab4a420a30b
Franck <franckfongang99@gmail.com>
2024-06-01 04:23:52 +0000

    Time: 1 ms (--%), Space: 42.4 MB (--%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    int[] memo;
+    public int minCostClimbingStairs(int[] cost) {
+        memo = new int[cost.length];
+        Arrays.fill(memo, -1);
+        return Math.min(helper(cost, cost.length - 1), helper(cost, cost.length - 2));
+    }
+
+    private int helper(int[] cost, int i) {
+        if (i < 0) return 0;
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        memo[i] = cost[i] + Math.min(helper(cost, i - 1), helper(cost, i - 2));
+        return memo[i];
+    }
+}
