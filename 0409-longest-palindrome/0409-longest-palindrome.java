a12ede6a5b02242bd490623edcfc3bcfadf3e28d
Franck <franckfongang99@gmail.com>
2024-06-04 18:53:45 +0000

    Time: 9 ms (--%), Space: 41.5 MB (--%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int result = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> it : map.entrySet()) {
+            if(it.getValue() % 2 == 0) result += it.getValue();
+            else result += it.getValue() - (it.getValue() % 2);
+        }
+        
+        if(result < s.length()) result++;
+    
+        return result;
+    }
+}
