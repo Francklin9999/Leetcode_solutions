3b73a51753f7d272f9e2b5dc722c75859c2d6a1e
Franck <franckfongang99@gmail.com>
2024-07-09 06:27:02 +0000

    Time: 3 ms (--%), Space: 73.1 MB (--%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,19 +1,17 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        int n = customers.length;
         double waitingTime = 0;
         double totalTime = customers[0][0];
 
-        for(int i = 0; i < n; i++) {
-            if(totalTime < customers[i][0]) totalTime = customers[i][0];
-            double distance = customers[i][1];
-            if (customers[i][0] < totalTime) {
-                distance += totalTime - customers[i][0];
+        for(int i = 0; i < customers.length; i++) {
+            waitingTime += customers[i][1];
+            if(totalTime <= customers[i][0]) totalTime = customers[i][0];
+            else {
+                waitingTime += totalTime - customers[i][0];
             }
-            waitingTime += distance;
             totalTime += customers[i][1]; 
         }
 
-        return (waitingTime / n);
+        return (waitingTime / customers.length);
     }
 }
