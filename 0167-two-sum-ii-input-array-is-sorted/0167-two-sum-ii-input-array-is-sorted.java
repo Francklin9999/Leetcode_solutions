6896ca39f2e71c44155c7832c8d3b213cb12ee99
Franck <franckfongang99@gmail.com>
2025-11-15 06:25:28 +0000

    Time: 2 ms (--%), Space: 48.3 MB (--%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,15 +3,14 @@ class Solution {
         int l = 0;
         int r = numbers.length - 1;
 
-        while ( l < r) {
-            if (numbers[l] + numbers[r] > target) {
-                r--;
-            } else if (numbers[l] + numbers[r] < target) {
-                l++;
-            } else {
+        while(l < r) {
+            int currSum = numbers[l] + numbers[r];
+            if (currSum == target) {
                 return new int[] {l + 1, r + 1};
-            }
+            } else if (currSum > target) r--;
+            else l++;
         }
-        return new int[2];
+
+        return new int[] {};
     }
 }
