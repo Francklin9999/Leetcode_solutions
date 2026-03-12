c50b15fb67d623f414a5e68bbe31c1ea6b2a849b
Franck <franckfongang99@gmail.com>
2026-03-12 07:05:52 +0000

    Time: 0 ms (--%), Space: 8.6 MB (--%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        unordered_map<int, int> map;
-        return dfs(0, s, map);
-    }
+        int next1 = 1;
+        int next2 = 0;
+        int curr = 0;
 
-    int dfs(int i, string s, unordered_map<int, int>& map) {
-        if (map[i]) return map[i];
-        if (i > s.size()) return 0;
-        if (i == s.size()) return 1;
-        if (s.at(i) == '0') return 0;
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s.at(i) == '0') {
+            } else {
+                curr += next1;
+                int num = std::stoi(s.substr(i, 2));
+                if (num <= 26 && num >= 10) curr += next2;
+            }
+            next2 = next1;
+            next1 = curr;
+            curr = 0;
+        }
 
-        int first = dfs(i + 1, s, map);
-        int sec = 0;
-        if (std::stoi(s.substr(i, 2)) <= 26)
-            sec = dfs(i + 2, s, map);
-        map[i] = first + sec;
-        return first + sec;
+        return next1;
     }
 };
