97802909c2a392d02c76ddc1232dd19ecdbb36aa
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-06-24 15:56:45 -0400

    Time: 0 ms (100%), Space: 31.4 MB (7.13%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-
+        int l{0};
+        int r{static_cast<int>(nums.size()) - 1};
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (target < nums[mid]) r = mid - 1;
-            else if (target > nums[mid]) l = mid + 1;
+            if (nums[mid] > target) r = mid - 1;
+            else if (nums[mid] < target) l = mid + 1;
             else return mid;
         }
 
