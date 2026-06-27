629923241a3771aa8a44915d3d2b924f4eafa604
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-06-27 18:15:57 -0400

    Time: 0 ms (100%), Space: 27.3 MB (52.2%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0;
+        int r = s.size() - 1;
+        int tmp;
+
+        while (l < r) {
+            tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+};
