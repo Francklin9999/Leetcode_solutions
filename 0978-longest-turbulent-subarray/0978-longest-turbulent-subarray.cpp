c96a7b352d14b4a615f5d62df93d873386a65811
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-07-16 01:21:12 -0400

    Time: 0 ms (100%), Space: 44.2 MB (28.11%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -4,19 +4,16 @@ public:
         int max{};
         int curr{1};
         bool greater{};
-        bool hasSeen{false};
 
         for (auto i = 1uz; i < arr.size(); ++i) {
             max = std::max(max, curr);
             if (arr[i] > arr[ i - 1]) {
-                if (!hasSeen || !greater) curr++;
+                if (!greater) curr++;
                 else curr = 2;
-                hasSeen = true;
                 greater = true;
             } else if (arr[i] < arr[ i - 1]) {
-                if  (!hasSeen || greater) curr++;
+                if  (greater) curr++;
                 else curr = 2;
-                hasSeen = true;
                 greater = false;
             } else {
                 curr = 1;
