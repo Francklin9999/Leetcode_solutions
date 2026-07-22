38da2a858003e16c952293565bff5ab04afc4f80
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-07-21 21:05:29 -0400

    Time: 11 ms (5.73%), Space: 51.4 MB (89.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,32 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        int count = 0;
-        int ROWS = grid.length;
-        int COLMN = grid[0].length;
-        int[][] neighbours = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLMN; j++) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        int total = 0;
+        int[][] nei = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '0') continue;
-                count++;
-                int[] pair = new int[2];
-                pair[0] = i;
-                pair[1] = j;
-                queue.add(pair);
+                queue.offer(new Pair<Integer, Integer>(i, j));
+                grid[i][j] = '0';
                 while (!queue.isEmpty()) {
-                    int[] curr = queue.poll();
-                    int r = curr[0];
-                    int c = curr[1];
-                    for (int [] neighbour : neighbours) {
-                        int newR = r + neighbour[0], newL = c + neighbour[1];
-                        if (Math.min(newR, newL) < 0 || newR == ROWS || newL == COLMN ||
-                        grid[newR][newL] == '0') continue;
-                        queue.offer(new int[]{newR, newL});
-                        grid[newR][newL] = '0';
+                    var top = queue.poll();
+                    for (var idx : nei) {
+                        var x = top.getKey() + idx[0];
+                        var y = top.getValue() + idx[1];
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != '1') continue;
+                        queue.offer(new Pair<Integer, Integer>(x, y));
+                        grid[x][y] = '0';
                     }
                 }
+                total++;
             }
         }
-        return count;
+
+        return total;
     }
 }
