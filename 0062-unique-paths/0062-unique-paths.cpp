ccd50f722f4d2e35c863f1e70ecbe69e7d441d9c
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-07 23:20:22 -0400

    Time: 0 ms (100%), Space: 9.4 MB (55.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> grid(m, vector<int>(n));
-        grid[m - 1][n - 1] = 1;
+        vector<vector<int>> res(m + 1, vector<int>(n + 1));
+        res[0][1] = 1;
 
-        for  (int i = grid.size() - 1; i >= 0; i--) {
-            for (int j = grid[0].size() - 1; j >= 0; j--) {
-                if (i == m - 1 && j == n - 1) continue;
-                grid[i][j] = 0;
-                int col = j + 1; 
-                if (col < grid[0].size()) grid[i][j] += grid[i][col];
-                int row = i + 1; 
-                if (row < grid.size()) grid[i][j] += grid[row][j];
+        for (auto i = 1uz; i <= m; ++i) {
+            for (auto j = 1uz; j <= n; ++j) {
+                res[i][j] = res[i - 1][j] + res[i][j - 1];
             }
         }
 
-        return grid[0][0];        
+        return res[m][n];
     }
 };
