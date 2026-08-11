f3e662bd5e0f42e36339cd4c3667b7151555818e
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-11 12:53:47 -0400

    Time: 94 ms (42.08%), Space: 127.3 MB (52.02%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -2,43 +2,65 @@ class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         int direction{1};
-        int x{0}, y{0};
-        int total{};
+        int x{};
+        int y{};
+        int max{0};
 
-        set<pair<int, int>> set;
-
-        for (auto& obj : obstacles) {
-            set.insert({obj[0], obj[1]});
+        set<pair<int, int>> obstacle;
+        
+        for (auto i = 0uz; i < obstacles.size(); ++i) {
+            obstacle.insert({pair<int, int>(obstacles[i][0], obstacles[i][1])});
         }
 
         for (auto i = 0uz; i < commands.size(); ++i) {
             if (commands[i] < 0) {
-                if (commands[i] == -2) direction++;
-                if (commands[i] == -1) direction--;
+                if (commands[i] == -2) {
+                    direction++;
+                } else {
+                    direction--;
+                }
                 direction = (direction + 4) % 4;
             } else {
-                if (direction == 0) {
-                    while (commands[i]-- > 0 && !set.contains({x + 1, y})) {
-                        x++;
+                auto steps = commands[i];
+                switch (direction) {
+                    case 0:
+                    {
+                        while (steps-- > 0 && !obstacle.count(pair<int, int>(x + 1, y))) {
+                            x++;
+                            max = std::max(max, x * x + y * y);
+                        }
+                        continue;
                     }
-                } else if (direction == 2) {
-                    while (commands[i]-- > 0 && !set.contains({x - 1, y})) {
-                        x--;
+                    case 1:
+                    {
+                        while (steps-- > 0 && !obstacle.count(pair<int, int>(x, y + 1))) {
+                            y++;
+                            max = std::max(max, x * x + y * y);
+                        }
+                        continue;
                     }
-                } else if (direction == 1) {
-                    while (commands[i]-- > 0 && !set.contains({x, y + 1})) {
-                        y++;
+                    case 2:
+                    {
+                        while (steps-- > 0 && !obstacle.count(pair<int, int>(x - 1, y))) {
+                            x--;
+                            max = std::max(max, x * x + y * y);
+                        }
+                        continue;
                     }
-                } else if (direction == 3) {
-                    while (commands[i]-- > 0 && !set.contains({x, y - 1})) {
-                        y--;
+                    case 3:
+                    {
+                        while (steps-- > 0 && !obstacle.count(pair<int, int>(x, y - 1))) {
+                            y--;
+                            max = std::max(max, x * x + y * y);
+                        }
+                        continue;
                     }
+                    default:
+                        continue;
                 }
             }
-
-            total = std::max(total, static_cast<int>(std::pow(std::abs(x), 2) + std::pow(std::abs(y), 2)));
         }
 
-        return total;
+        return max;
     }
 };
