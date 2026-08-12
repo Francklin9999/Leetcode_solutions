47e75ab755922700be2ac5894fa559390a25fa6e
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-11 21:51:28 -0400

    Time: 4 ms (28.56%), Space: 8.1 MB (94.37%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int reverseBits(int n) {
         int result{};
-        int m = n;
 
         for (auto i = 0; i < 32; ++i) {
-            result = (result << 1) + (m & 1);
-            m = m >> 1;
+            result = (result << 1) | (n & 1);
+            n = n >> 1;
         }
 
         return result;
