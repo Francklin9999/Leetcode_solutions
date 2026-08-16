7ed550695c3cdbfe55b21c20ad489fa9c10a139f
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-16 15:10:42 -0400

    Time: 0 ms (100%), Space: 20 MB (84.92%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy{-prices[0]}, sell{};
+
+        for (auto i{1uz}; i < prices.size(); ++i) {
+            auto t_buy = buy;
+            auto t_sell = sell;
+
+            buy = std::max(t_sell - prices[i], t_buy); 
+            sell = buy + prices[i];
+
+            t_buy = buy;
+            t_sell = sell;
+
+            buy = std::max(t_sell - prices[i], t_buy); 
+            sell = buy + prices[i];
+        }
+
+        return sell > 0 ? sell : 0;
+    }
+};
