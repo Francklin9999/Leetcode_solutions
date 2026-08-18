2bf4dd58e68fbf8951532a43a87b7a437cff5ccb
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-18 18:47:30 -0400

    Time: 0 ms (100%), Space: 9.2 MB (76.15%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<uint> dp(target + 1, 0);
+        vector<uint> dp(target + 1);
         dp[0] = 1;
 
-        for (int i = 0; i <= target; ++i) {
+        for (int i = 1; i <= target; ++i) {
             for (int num : nums) {
-                auto res = i - num;
-                if (res >= 0) {
-                    dp[i] += dp[res];
+                if (i >= num) {
+                    dp[i] += dp[i - num];
                 }
             }
         }
