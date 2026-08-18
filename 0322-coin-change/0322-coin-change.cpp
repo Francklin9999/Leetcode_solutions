10af554db935604165107d667d404cfdd2209e3f
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-18 18:03:32 -0400

    Time: 19 ms (81.03%), Space: 17.8 MB (84.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
 
-        for (int i{}; i <= amount; ++i) {
+        for (int i{0}; i <= amount; ++i) {
             for (int coin : coins) {
-                if (i - coin < 0 || dp[i - coin] == INT_MAX) continue;
-                dp[i] = std::min(dp[i], dp[i - coin] + 1);
+                auto res = i - coin;
+                if (res >= 0) {
+                    dp[i] = std::min(dp[i], dp[res] + 1);
+                }
             }
         }
 
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 };
