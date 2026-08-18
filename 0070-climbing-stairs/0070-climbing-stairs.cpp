9a977cf5518bf4c6db0a86ab8781c6122cbcd9b0
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-18 15:05:35 -0400

    Time: 0 ms (100%), Space: 7.7 MB (88.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int prev1{1}, prev2{0};
+        int prev1{}, prev2{1};
 
-        for (int i{}; i < n; ++i) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        for (auto i{0}; i <= n; ++i) {
+            auto tmp = prev1;
+            prev1 = prev1 + prev2;
+            prev2 = tmp;
         }
 
         return prev1;
