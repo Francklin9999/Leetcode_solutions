17108f4d3ea9857c8575a0d79b1ab9a5a1b1c2bd
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-18 15:41:21 -0400

    Time: 0 ms (100%), Space: 10.2 MB (74.96%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
-public:
-    int rob(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return std::max(nums[0], 0);
-        }
-        return std::max(
-            helper(nums, 0, nums.size() - 1),
-            helper(nums, 1, nums.size())
-        );
+int helper(vector<int>& nums, int l, int r) {
+    int prev1{}, prev2{};
+
+    for (auto i{l}; i < r; ++i) {
+        auto tmp = prev1;
+        prev1 = std::max(prev1, prev2 + nums[i]);
+        prev2 = tmp;
     }
 
-    int helper(vector<int>& nums, int i, int j) {
-        int prev1{}, prev2{};
+    return prev1;
+}
 
-        for (auto k = i; k < j; ++k) {
-            int tmp = prev1;
-            prev1 = std::max(prev1, prev2 + nums[k]);
-            prev2 = tmp;
-        }
-
-        return std::max(prev1, prev2);
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        return std::max(helper(nums, 1, nums.size()), helper(nums, 0, nums.size() - 1));
     }
 };
