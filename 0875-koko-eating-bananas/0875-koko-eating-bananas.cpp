a5359be293a9660407cf61d72bfdab94fbe676c8
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-20 16:13:12 -0400

    Time: 7 ms (69.57%), Space: 23 MB (56.86%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,29 @@
 class Solution {
+bool isPossible(vector<int>& piles, int h, int speed) {
+    int hours{};
+
+    for (auto i{0uz}; i < piles.size(); ++i) {
+        hours += piles[i] / speed;
+        if (piles[i] % speed != 0) ++hours;
+    }
+
+    return hours <= h;
+}
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int max = *std::max_element(piles.begin(), piles.end());
-        int min{max};
-
         int l{1};
-        int r{max};
+        int r{*std::max_element(piles.begin(), piles.end())};
 
-        while (l <= r) {
+        while (l < r) {
             int mid = l + (r - l) / 2;
 
-            if (helper(piles, h, mid)) {
-                min = mid;
-                r = mid - 1;
+            if (isPossible(piles, h, mid)) {
+                r = mid;
             } else {
                 l = mid + 1;
             }
         }
 
-        return min;
-    }
-
-    bool helper(vector<int>& piles, int h, int mid) {
-        double hours{};
-
-        for (auto i = 0uz; i < piles.size(); ++i) {
-            hours += std::ceil((double) piles[i] / (double) mid);
-        }
-
-        return hours <= h;
+        return r;
     }
 };
