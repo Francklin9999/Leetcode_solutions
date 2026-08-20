81f0c46e8dd847a72b72a98e75f0c66116bc4c07
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-20 15:00:20 -0400

    Time: 0 ms (100%), Space: 10.9 MB (41.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(26, 0);
+        int count[26] = {0};
 
-        int l{}, r{};
+        int l{}, r{}, m{};
         int n = s.size();
         int longest{1};
 
         for (; r < n; ++r) {
             ++count[s[r] - 'A'];
 
-            int m = *std::max_element(count.begin(), count.end());
+            m = std::max(m, count[s[r] - 'A']);
 
             if (r - l + 1 - m > k) {
                 longest = std::max(longest, r - l);
