f49444143c89b561f86c647ab1d2c771c68538c6
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-21 01:23:26 -0400

    Time: 0 ms (100%), Space: 11.9 MB (58%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        auto prefix{strs[0]};
-
+        string pre{strs[0]};
+        
         for (auto i{1uz}; i < strs.size(); ++i) {
-            int l{};
-            int min = std::min(prefix.size(), strs[i].size());
-            while (l < min && prefix[l] == strs[i][l]) l++;
-            prefix = prefix.substr(0, l);
+            auto j{0uz};
+            auto m = std::min(pre.size(), strs[i].size());
+            for (; j < m; ++j) {
+                if (pre[j] != strs[i][j]) break;
+            }
+
+            pre = pre.substr(0, j);
+            if (pre == "") return "";
         }
 
-        return prefix;
+        return pre;
     }
 };
