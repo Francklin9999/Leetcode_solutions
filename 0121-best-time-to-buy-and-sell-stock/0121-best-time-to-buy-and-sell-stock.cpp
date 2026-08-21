f20055acb91ce39c737db48ccf1b8d0e623aca7b
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-20 22:16:02 -0400

    Time: 0 ms (100%), Space: 97.2 MB (87.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy{prices[0]};
-        int profit{};
+        int buy{prices[0]}, profit{};
 
         for (auto i{1uz}; i < prices.size(); ++i) {
-            buy = std::min(buy, prices[i]);
             profit = std::max(profit, prices[i] - buy);
+            buy = std::min(buy, prices[i]);
         }
 
         return profit;
