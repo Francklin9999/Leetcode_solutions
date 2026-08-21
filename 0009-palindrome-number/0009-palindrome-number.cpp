d03a8b06500a4fde3b3cd71a91e919d96d89594c
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-20 23:26:29 -0400

    Time: 3 ms (43.83%), Space: 8.6 MB (64.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,14 +3,22 @@ public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
 
-        int org = x;
-        long long num = 0;
+        int cp = x;
+        long m{};
 
-        while (org != 0) {
-            num = num * 10 + org % 10;
-            org = org / 10;
+        while (cp > 0) {
+            m *= 10;
+            m += cp % 10;
+            cp /= 10;
         }
 
-        return num == x;
+        while (m > 0 && x > 0) {
+            if (m % 10 != x % 10) return false;
+
+            m /= 10;
+            x /= 10;
+        }
+
+        return true;
     }
 };
