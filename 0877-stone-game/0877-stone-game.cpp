3d671b7f5b67da3faff3629dd1d1fd4503afdeae
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-25 14:35:08 -0400

    Time: 16 ms (12.17%), Space: 19.6 MB (21.25%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,29 +1,19 @@
 class Solution {
+int helper(vector<int>& piles, vector<vector<int>>& dp, int l, int r) {
+    if (l > r) return 0;
+    if (dp[l][r] != INT_MIN) return dp[l][r];
+
+    dp[l][r] = std::max(
+        piles[l] - helper(piles, dp, l + 1, r),
+        piles[r] - helper(piles, dp, l, r - 1)
+    );
+
+    return dp[l][r];
+}
 public:
     bool stoneGame(vector<int>& piles) {
-        int alice{}, bob{};
-        int l = 0, r = piles.size() - 1;
-        bool turn{true};
+        vector<vector<int>> dp(piles.size(), vector<int>(piles.size(), INT_MIN));
 
-        while (l <= r) {
-            auto left = piles[l];
-            if (l + 1 <= r) left += piles[l + 1];
-
-            auto right = piles[r];
-            if (r - 1 >= l) right += piles[r - 1];
-
-            auto& p = turn ? alice : bob;
-            turn = !turn;
-
-            if (left > right) {
-                p += left;
-                ++l;
-            } else {
-                p += right;
-                --r;
-            }
-        }
-
-        return alice > bob;
+        return helper(piles, dp, 0, piles.size() - 1) > 0;
     }
 };
