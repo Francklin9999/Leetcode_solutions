229859b4b5ca041035d62a31e723b663dbd4be96
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-24 23:52:05 -0400

    Time: 41 ms (74.94%), Space: 13.2 MB (71.05%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -5,10 +5,8 @@ public:
         dp[0] = 0;
 
         for (int i{1}; i <= n; ++i) {
-            for (int j{1}; j <= n / 2 + 1; ++j) {
-                if (i >= j * j) {
-                    dp[i] = std::min(dp[i], dp[i - j * j] + 1);
-                }
+            for (int j{1}; j * j <= i; ++j) {
+                dp[i] = std::min(dp[i], dp[i - j * j] + 1);
             }
         }
 
