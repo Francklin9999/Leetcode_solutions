fff41d1a1f72a04a5c94bf5f11dcb412974cbe5d
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-24 23:50:51 -0400

    Time: 876 ms (5.02%), Space: 13.1 MB (85.44%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int numSquares(int n) {
-        unordered_set<int> set;
-
-        for (int i{}; i < n / 2 + 1; ++i) {
-            set.insert(i * i);
-        }
-
         vector<int> dp(n + 1, n);
         dp[0] = 0;
 
-        for (int i{}; i <= n; ++i) {
-            for (auto& val : set) {
-                if (i >= val) {
-                    dp[i] = std::min(dp[i], dp[i - val] + 1);
+        for (int i{1}; i <= n; ++i) {
+            for (int j{1}; j <= n / 2 + 1; ++j) {
+                if (i >= j * j) {
+                    dp[i] = std::min(dp[i], dp[i - j * j] + 1);
                 }
             }
         }
