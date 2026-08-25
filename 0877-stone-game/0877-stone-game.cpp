13eeb92a6060ac384353c8f6947ec45413d3f93f
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-25 13:38:52 -0400

    Time: 0 ms (100%), Space: 10.5 MB (42.48%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int alice{}, bob{};
+        int l = 0, r = piles.size() - 1;
+        bool turn{true};
+
+        while (l <= r) {
+            auto left = piles[l];
+            if (l + 1 <= r) left += piles[l + 1];
+
+            auto right = piles[r];
+            if (r - 1 >= l) right += piles[r - 1];
+
+            auto& p = turn ? alice : bob;
+            turn = !turn;
+
+            if (left > right) {
+                p += left;
+                ++l;
+            } else {
+                p += right;
+                --r;
+            }
+        }
+
+        return alice > bob;
     }
 };
