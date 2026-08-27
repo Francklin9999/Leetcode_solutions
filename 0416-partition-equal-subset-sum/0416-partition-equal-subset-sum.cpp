cc4967e3ec99580924cf44dbff83d3c622fc7ed0
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-27 13:27:50 -0400

    Time: 99 ms (72.17%), Space: 204.1 MB (8.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,30 +3,31 @@ bool helper(vector<int>& nums, vector<vector<int>>& curr, int idx, int sum, int 
     if (sum == total) return true;
     if (sum > total) return false;
     if (idx == nums.size()) return false;
+
     if (curr[idx][sum] != - 1) return curr[idx][sum];
 
-    bool take = helper(
+    if (helper(
         nums,
         curr,
         idx + 1,
         sum + nums[idx],
         total
-    );
+    )) {
+        return curr[idx][sum] = true;
+    }
 
-    bool leave = helper(
+    return curr[idx][sum] = helper(
         nums,
         curr,
         idx + 1,
         sum,
         total
     );
-
-    return curr[idx][sum] = take || leave;
 }
 public:
     bool canPartition(vector<int>& nums) {
         auto sum = std::accumulate(nums.begin(), nums.end(), 0);
-        if (sum & 1 != 0) return false;
+        if (sum % 2 != 0) return false;
         vector<vector<int>> curr(nums.size(), vector<int>(sum + 1, -1));
         return helper(nums, curr, 0, 0,  sum / 2);
     }
