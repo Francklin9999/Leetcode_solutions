629b46371c180e2d3a13fbf7a6434002d6938813
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-27 22:38:04 -0400

    Time: 0 ms (100%), Space: 15.7 MB (85.66%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        map<int, int> freq;
+        int m = *std::max_element(nums.begin(), nums.end());
+        
+        vector<int> freq(m + 1, 0);
+        vector<int> dp(m + 1);
 
         for (int num : nums) ++freq[num];
 
-        int m = *std::max_element(nums.begin(), nums.end());
-
-        vector<int> dp(m + 1);
         dp[0] = 0;
+        dp[1] = freq[1];
 
         int res{};
 
-        for (auto i{1uz}; i < dp.size(); ++i) {
-            if (i > 1) dp[i] = dp[i - 2];
-            
-            auto it = freq.find(i);
-            if (it != freq.end()) dp[i] += (it->first * it->second);
-
-            dp[i] = std::max(dp[i], dp[i - 1]);
-
-            res = std::max(res, dp[i]);
+        for (int i{2}; i <= m; ++i) {
+            dp[i] = std::max(
+                dp[i - 1],
+                dp[i - 2] + i * freq[i]
+            );
         }
 
-        return res;
+        return dp[m];
     }
 };
