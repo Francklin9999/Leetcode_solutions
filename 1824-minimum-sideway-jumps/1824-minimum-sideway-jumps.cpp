3d128a1a5ba728c2c7e988a6eac61d847edc38fb
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-30 03:00:21 -0400

    Time: 28 ms (92.27%), Space: 191.8 MB (92.27%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int minSideJumps(vector<int>& obstacles) {
-        int lanes[] = {1, 0, 1};
+        int lanes[3] = {1, 0, 1};
 
         for (int obs : obstacles) {
             if (obs) lanes[obs - 1] = INT_MAX;
 
             auto best = std::min({lanes[0], lanes[1], lanes[2]});
-
-            for (int i{0}; i < 3; ++i) {
-                if (obs - 1 == i) continue;
-
+            for (int i{}; i < 3; ++i) {
+                if (i == obs - 1) continue;
                 lanes[i] = std::min(lanes[i], best + 1);
             }
         }
