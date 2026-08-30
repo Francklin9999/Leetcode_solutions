30aa82420480f946e83caeecffe3c8b09ca85cb2
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-30 01:55:16 -0400

    Time: 0 ms (100%), Space: 17.4 MB (97.61%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int prev1{cost[1]}, prev2{cost[0]};
+        int prev1{}, prev2{};
 
-        for (auto i{2uz}; i < cost.size(); ++i) {
+        for (auto i{0uz}; i < cost.size(); ++i) {
             auto tmp = prev1;
             prev1 = std::min(prev1, prev2) + cost[i];
             prev2 = tmp;
         }
-        
+
         return std::min(prev1, prev2);
     }
 };
