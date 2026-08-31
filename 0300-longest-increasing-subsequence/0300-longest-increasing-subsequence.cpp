d8e356af63ceed440ee61cbe5f1b9897e5f7f309
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-30 21:55:10 -0400

    Time: 59 ms (64.88%), Space: 14.4 MB (61.63%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int res{};
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), 1);
-        for (auto i = nums.size(); i-- > 0; ) {
-            for (auto j = i + 1; j < nums.size(); ++j) {
+        vector<int> dp(nums.size() + 1, 1);
+
+        for (auto i{nums.size()}; i-- > 0; ) {
+            for (auto j{i + 1}; j < nums.size(); ++j) {
                 if (nums[i] < nums[j]) {
                     dp[i] = std::max(dp[i], dp[j] + 1);
                 }
