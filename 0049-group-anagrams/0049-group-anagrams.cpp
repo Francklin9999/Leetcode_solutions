92165247a24ba93cefb7ecaeafb894555284462c
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2026-08-30 20:48:52 -0400

    Time: 12 ms (83.26%), Space: 25 MB (82.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> group;
+        unordered_map<string, vector<string>> groups;
 
         for (auto i{0uz}; i < strs.size(); ++i) {
-            auto s = strs[i];
-
-            sort(s.begin(), s.end());
-
-            group[s].push_back(strs[i]);
+            auto tmp = strs[i];
+            sort(tmp.begin(), tmp.end());
+            groups[tmp].push_back(strs[i]);
         }
 
         vector<vector<string>> res;
-        res.reserve(group.size());
 
-        for (auto& [key, value] : group) {
+        for (auto& [_, value] : groups) {
             res.push_back(value);
         }
 
